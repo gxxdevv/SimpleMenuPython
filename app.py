29ebbcd967fad815d69c144768d3c6352aec628e
Gustavo de Oliveira Guilherme <gxxdevv@gmail.com>
2026-04-05 15:57:24 -0300

    Refactor joguinho function for clarity and flow

--- app.py
+++ app.py
@@ -39,37 +39,33 @@ def calculadora():
 
 def joguinho():
     while True:
-        #define o número aleatório do jogo
+        
         num_jog = random.randint(1,1000)
         
-        #serve para informar ao jogador o intervalo de números
-        print("número natural(0,1,2,...) entre 1 e 1000")
-        
-        while True: #loop de tentativas
-            try: #significa tentar. Então, o usuário só pode digitar números
-                opcao_jog = int(input("Vamos ver se você acerta o número: "))    
-            except ValueError: #isso impede do usuário digitar letras   
+        while True: 
+            try: 
+                opcao_jog = int(input("Vamos ver se você acerta o número: "))
+            except ValueError:  
                 print("digite apenas números!")
-                continue #isso continua o código normalmente
+                continue    
+
+            if opcao_jog > num_jog:
+                print("número menor")
+                continue
+            elif opcao_jog < num_jog:
+                print("número maior")
+                continue
+            else:
+                print("ACERTOU!")
                 
-                #serve como forma de dica para o jogador
-                if opcao_jog > num_jog:
-                    print("número menor")
-                elif opcao_jog < num_jog:
-                    print("número maior")
-                else:
-                    print("ACERTOU!!")
-                    break
-                
-            #pergunta seo jogador deseja jogar mais uma partida
-            opcao2 = input("deseja jogar mais uma rodada?")   
-            if opcao2.strip().lower() in ["não","nao"]:
-                print("Adeus, volte sempre!")
-                break
+                opcao2 = input("deseja jogar mais uma rodada?") 
               
-def adeus():
-    print("Muito obrigado por usar o programa. Adeus!")
-      
+                if opcao2.strip().lower() in ["não","nao"]:
+                    print("Adeus, volte sempre!")
+                    return 
+                else:
+                    break
+               
 def menu():
     while True:
         print("Bem-vindo(a)!")
@@ -78,15 +74,15 @@ def menu():
         print("2) jogar um joguinho?")
         print("3) sair do programa?")
         
-        opcao_menu = float(input("digite 1, 2 ou 3: "))
-        if opcao_menu == 1:
+        opcao_menu = input("digite 1, 2 ou 3: ")
+        if opcao_menu == "1":
             calculadora()
-        elif opcao_menu == 2:
+        elif opcao_menu == "2":
             joguinho()
-        elif opcao_menu == 3:
-            adeus()
+        elif opcao_menu == "3":
+            print("Muito obrigado por usar o programa. Adeus!")
             break
-        elif opcao_menu == 3.14:
+        elif opcao_menu == "3.14":
             print("Calma... PI?")
             print("3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679")
             print("Gustavo diz:  Muito obrigado a todos que usaram o programa, que Deus abençoe a vida de vocês!")
