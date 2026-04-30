f21df56c902ffafadb60f6c0ee143621be598314
Gustavo de Oliveira Guilherme <gxxdevv@gmail.com>
2026-04-29 23:17:33 -0300

    Refactor calculator and add new game features

--- app.py
+++ app.py
@@ -2,29 +2,43 @@ import random
 
 def calculadora():
     while True:
+        print("\n=====CALCULADORA=====")
         print("+ soma")
         print("- subtração")
         print("/ divisão")
         print("* multiplicação")
+        print("** exponenciação")
         
-        num1 = float(input("digite um número: "))
-        num2 = float(input("digite outro número: "))
-        
+        try:
+            num1 = float(input("digite um número: "))
+            num2 = float(input("digite outro número: "))
+        except ValueError:
+            print("Só números")
+            continue
+
         operacao = input("Que tipo de conta deseja fazer? ")
         
         match operacao:
             case "+":
                 res = num1 + num2
+
             case "-":
                 res = num1 - num2
+
             case "*":
                 res = num1 * num2
+
             case "/":
                 if num2 == 0:
                     print("não existe divisão por 0")
                     continue
                 else:
                     res = num1 / num2
+                    print(f"o resto dessa divisão é {num1 % num2}")
+
+            case "**":
+                res = num1 ** num2
+
             case _:
                 print("operação inválida")
                 continue
@@ -37,10 +51,10 @@ def calculadora():
             print("adeus!")
             break
 
-def joguinho():
+def adivinhacao():
   while True:
     num = random.randint(1,1000)
-    
+    print("\n=====GAME=====")
     contador = 10
     for i in range(contador):
         try:
@@ -67,28 +81,62 @@ def joguinho():
     if opcao2.strip().lower() in ["não", "nao"]:
         print("Valeu por jogar!")
         break
-               
+
+def game():
+    opcoes = ["pedra", "papel", "tesoura"]
+
+    print("\n=====GAME=====")
+    print("Olá jogador(a), vamos jogar pedra, papel e tesoura")
+
+    while True:
+        computador = random.choice(opcoes)
+        jogador = input("Escolha: ").lower().strip()
+
+        if jogador not in opcoes:
+            print("Opção inválida")
+            continue
+
+        print(f"Computador escolheu {computador}")
+        print(f"Você escolheu {jogador}")
+
+        if jogador == computador:
+            print("Empate!")
+        elif (jogador == "pedra" and computador == "tesoura") or \
+            (jogador == "papel" and computador == "pedra") or \
+            (jogador == "tesoura" and computador == "papel"):
+            print("Você venceu!")
+        else:
+            print("Você Perdeu!")
+
+        pergunta = input("Deseja jogar de novo? ").lower().strip()
+
+        if pergunta.strip().lower() in ["não", "nao"]:
+            print("Adeus")
+            break 
+          
 def menu():
     while True:
-        print("Bem-vindo(a)!")
-        print("O que deseja fazer?")
+        print("\n=====MENU=====")
         print("1) usar a calculadora?")
-        print("2) jogar um joguinho?")
-        print("3) sair do programa?")
+        print("2) jogar um jogo de adivinhação?")
+        print("3) Jogar Pedra, papel e tesoura?")
+        print("4) sair do programa?")
         
-        opcao_menu = input("digite 1, 2 ou 3: ")
-        if opcao_menu == "1":
+        try:
+            opcao_menu = int(input("Digite sua escolha: "))
+        except ValueError:
+            print("Somente núneros")
+            continue
+
+        if opcao_menu == 1:
             calculadora()
-        elif opcao_menu == "2":
-            joguinho()
-        elif opcao_menu == "3":
-            print("Muito obrigado por usar o programa. Adeus!")
+        elif opcao_menu == 2:
+            adivinhacao()
+        elif opcao_menu == 3:
+            game()
+        elif opcao_menu == 4:
+            print("Adeus!")
             break
-        elif opcao_menu == "3.14":
-            print("Calma... PI?")
-            print("3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679")
-            print("Gustavo diz:  Muito obrigado a todos que usaram o programa, que Deus abençoe a vida de vocês!")
-            break
-    
+ 
 if __name__ == "__main__":
     menu()
