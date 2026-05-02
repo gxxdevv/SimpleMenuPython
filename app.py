4d1edc1886d0b400f27ad24939c159da29298c5e
Gustavo de Oliveira Guilherme <gxxdevv@gmail.com>
2026-05-02 00:43:36 -0300

    Add currency converter and refactor menu structure

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
 import random 
 
+def conversor():
+    moedas ={
+        "usd": 5.0,
+        "eur": 5.5,
+        "gbp": 6.7,
+    }
+    while True:
+        print("\n=====CONVERSOR=====") 
+        try:
+            valor = float(input("digite o valor a ser convertido em BRL: R$"))
+        except ValueError:
+            print("só números")
+            continue
+
+        moeda = input("Escolha a moeda (USD, EUR, GBP): ").lower().strip()
+
+        if moeda not in moedas:
+            print("Moeda inválida")
+            continue
+        
+        resultado = valor / moedas[moeda]
+        print(f"Valor convertido: {resultado:.2f} {moeda.upper()}")
+        
+        pergunta = input("deseja fazer mais alguma comversão? ")
+        if pergunta.strip().lower() in ["não", "nao"]:
+            print("adeus!")
+            break
+
 def calculadora():
     while True:
         print("\n=====CALCULADORA=====")
@@ -37,7 +65,11 @@ def calculadora():
                     print(f"o resto dessa divisão é {num1 % num2}")
 
             case "**":
-                res = num1 ** num2
+                try:
+                    res = num1 ** num2
+                except OverflowError:
+                    print("número grande demais!")
+                    continue
 
             case _:
                 print("operação inválida")
@@ -52,35 +84,34 @@ def calculadora():
             break
 
 def adivinhacao():
-  while True:
-    num = random.randint(1,1000)
-    print("\n=====GAME=====")
-    contador = 10
-    for i in range(contador):
-        try:
-            opcao = int(input("Digite seu palpite: "))
-        except ValueError:
-            print("somente números!")
-            continue
+    while True: 
+        num_jog = random.randint(1,1000) 
+
+        for i in range(10):
+            print("\n=====ADIVINHAÇÃO=====")
+            try:
+                opcao_jog = int(input("Vamos ver se você acerta o número: ")) 
+            except ValueError:
+                print("Digite apenas números!")
+                continue
+            
+            if opcao_jog > num_jog: 
+                print("número menor") 
+            elif opcao_jog < num_jog: 
+                print("número maior") 
+            else: 
+                print("ACERTOU!!") 
+                break 
+            
+            print(f"tentativas restantes: {9 - i}")
         
-        if opcao > num:
-            print("número menor")
-            print(f"tentativas restantes: {contador}")
-        elif opcao < num:
-            print("número maior")
-            print(f"número de tentativas: {contador}")
         else:
-            print("ACERTOU!")
+            print("Perdeu!")
+
+        opcao2 = input("deseja jogar mais uma rodada?") 
+        if opcao2.strip().lower() in ["não","nao"]: 
+            print("Adeus, volte sempre!") 
             break
-        
-        print(f"tentativas restantes: {contador - i - 1}")
-    else:
-        print("Perdeu!")
-    
-    opcao2 = input("jogar novamente? ")
-    if opcao2.strip().lower() in ["não", "nao"]:
-        print("Valeu por jogar!")
-        break
 
 def game():
     opcoes = ["pedra", "papel", "tesoura"]
@@ -113,30 +144,76 @@ def game():
         if pergunta.strip().lower() in ["não", "nao"]:
             print("Adeus")
             break 
-          
-def menu():
+
+def jogos():
     while True:
-        print("\n=====MENU=====")
-        print("1) usar a calculadora?")
-        print("2) jogar um jogo de adivinhação?")
-        print("3) Jogar Pedra, papel e tesoura?")
-        print("4) sair do programa?")
-        
+        print("\n=====JOGOS=====")
+        print("1) jogo de adivinhação")
+        print("2) pedra, papel e tesoura")
+        print("3) sair")
+
         try:
-            opcao_menu = int(input("Digite sua escolha: "))
+            opcao_jogo = int(input("Escolha uma opção: "))
         except ValueError:
-            print("Somente núneros")
+            print("somente números")
             continue
 
-        if opcao_menu == 1:
-            calculadora()
-        elif opcao_menu == 2:
+        if opcao_jogo == 1:
             adivinhacao()
-        elif opcao_menu == 3:
+        elif opcao_jogo == 2:
             game()
-        elif opcao_menu == 4:
-            print("Adeus!")
+        elif opcao_jogo == 3:
             break
+        else:
+            print("Em Breve...")
+            continue
  
+def utilidades():
+    while True:
+        print("\n=====UTILIDADES=====")
+        print("1) calculadora")
+        print("2) conversor de moedas")
+        print("3) sair")
+
+        try:
+            opcao_utilidades = int(input("Escolha uma opção: "))
+        except ValueError:
+            print("somente números")
+            continue
+
+        if opcao_utilidades == 1:
+            calculadora()
+        elif opcao_utilidades == 2:
+            conversor()
+        elif opcao_utilidades == 3:
+            break
+        else:
+            print("Em Breve...")
+            continue
+
+def menu(): 
+    while True:
+        print("\n=====MENU=====")
+        print("1) Jogos")
+        print("2) Utilidades")
+        print("3) Sair")
+
+        try:
+           opcao_menu = int(input("Escolha uma opção: "))
+        except ValueError:
+           print("somente números")
+           continue
+
+        if opcao_menu == 1:
+            jogos()
+        elif opcao_menu == 2:
+            utilidades()
+        elif opcao_menu == 3:
+            print("adeus")
+            break
+        else:
+            print("Em breve...")
+            continue
+
 if __name__ == "__main__":
     menu()
